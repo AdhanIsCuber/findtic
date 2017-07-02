ca18658e239e3f9db9dd1b7cfdaa45502613829c
Wirya Ramadhan <AdhanIsCuber@gmail.com>
2017-07-03 00:41:58 +0700

    tambahin 404 page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ var index = express.Router();
 index.get('/', function(req, res) {
     res.render('user/index.html');
 });
+index.get('/404', function(req, res) {
+    res.render('user/404.html');
+});
 
 module.exports = index;
